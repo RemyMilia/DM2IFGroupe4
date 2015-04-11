b9a8ffa77b98aac2906e1d31c023698bf89b80c4
lm75 <laurentmary62@gmail.com>
2015-04-11 12:34:20 +0200

    Publication du header du composant 6

--- developpement/developpement/Composant6.h
+++ developpement/developpement/Composant6.h
@@ -1,3 +1,15 @@
-#include<vector>
+#pragma once
+#ifndef COMPOSANT6_H
+#define COMPOSANT6_H
 
-std::vector<double> getChemin();
+#ifdef COMPOSANT0_EXPORTS
+	#define COMPOSANT6_INTERFACE __declspec(dllexport)
+#else
+	#define COMPOSANT6_INTERFACE __declspec(dllimport)
+#endif
+
+COMPOSANT6_INTERFACE char * getComposant0Version();
+
+COMPOSANT6_INTERFACE vector<double> getChemin();
+
+#endif
