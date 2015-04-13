08bdeec82cdea7181503b0b9179a6dc85dd177b2
lm75 <laurentmary62@gmail.com>
2015-04-13 11:34:07 +0200

    Mise à jour header Composant6
    Version 1.3

--- test/testx/interfaces/Composant6.h
+++ test/testx/interfaces/Composant6.h
@@ -8,12 +8,15 @@
 	#define COMPOSANT6_INTERFACE __declspec(dllimport)
 #endif
 
+#include <vector>
+
+// obtenir la version du dll
 COMPOSANT6_INTERFACE char * getComposant6Version();
-
-COMPOSANT6_INTERFACE vector<double> getChemin();
-
-COMPOSANT6_INTERFACE vector<double> getN();
-
-COMPOSANT6_INTERFACE vector<double> getSigma();
+// obtenir le chemin genere
+COMPOSANT6_INTERFACE std::vector<double> getChemin();
+// obtenir la variable aleatoire N de moyenne 0 et variance 1
+COMPOSANT6_INTERFACE std::vector<double> getN();
+// obtenir la volatilite locale en t
+COMPOSANT6_INTERFACE std::vector<double> getSigma();
 
 #endif
